6f89e8be0eb0c188d817fb9699e66e0b6ce99eae
Dan <danjoaquinchin@gmail.com>
2018-08-02 11:50:05 +0800

    the thingy works

--- resources/views/tSP.blade.php
+++ resources/views/tSP.blade.php
@@ -3,21 +3,21 @@
 @section('content')
 <section class="content">
     <div class="container">
-        <div class="row">
-            <div class="col-4">
+        <div class="row col-md-12">
+            <div class="col-md-3">
                 <div class="list-group" id="list-tab" role="tablist">
-                <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
-                <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
-                <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
+                    <a class="list-group-item list-group-item-action active" id="list-ccs" data-toggle="list" href="#list-home" role="tab" aria-controls="CCS">CCS</a>
+                    @for( $i=0 ; $i<=12 ; $i++)
+                    <a class="list-group-item list-group-item-action" id=list-{{$i}} data-toggle="list" href=#{{$i}} role="tab" aria-controls={{$i}}>{{$i}}</a>
+                    @endfor
                 </div>
             </div>
-            <div class="col-8">
+            <div class="col-md-9">
                 <div class="tab-content" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">...</div>
-                <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
-                <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-                <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
+                <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">HOME</div>
+                <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">PROFILE</div>
+                <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">MSG</div>
+                <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">SETTINGS</div>
                 </div>
             </div>
         </div>

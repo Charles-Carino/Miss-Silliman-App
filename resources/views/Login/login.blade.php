d01a3311274fc31bafd61b8abf5395435428e28a
KuyaKoya <sethmcornelio@su.edu.ph>
2018-08-03 11:51:14 +0800

    login url changed

--- resources/views/Login/login.blade.php
+++ resources/views/Login/login.blade.php
@@ -57,7 +57,7 @@
           <p> LOGO HERE </p>
           <p class="p-t-35">Log into your pages account</p>
           <!-- START Login Form -->
-          <form id="form-login" class="p-t-15" method="POST" action="{{url('/signin/')}}">
+          <form id="form-login" class="p-t-15" method="POST" action="{{url('/')}}">
             <!-- START Form Control-->
             <div class="form-group form-group-default">
               <label>Username</label>
@@ -107,4 +107,3 @@
     </script>
   </body>
 </html>
-

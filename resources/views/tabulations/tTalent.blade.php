8448aee3d38e76d6ef262896a98b17e8119a4431
Dan <danjoaquinchin@gmail.com>
2018-08-03 12:46:04 +0800

    more changes

--- resources/views/tabulations/tTalent.blade.php
+++ resources/views/tabulations/tTalent.blade.php
@@ -1,3 +1,30 @@
+<?php
+    $collegeCode = ["CBA","CAS","MED","HS","CED","MASSCOMM","COPVA","GRAD","IRS","NURSING"];
+    $name = [
+        "Mikhaella",
+        "Christine",
+        "Oghogho",
+        "Erica",
+        "Shannel",
+        "Ivy",
+        "Chanel",
+        "Yihui",
+        "Amidala",
+        "Gabrielle"
+    ]; 
+    $picSrc = [
+        "public/css/images/CBA.png",
+        "public/css/images/CAS.png",
+        "public/css/images/MED.png",
+        "public/css/images/HS.png",
+        "public/css/images/CED.png",
+        "public/css/images/MC.png",
+        "public/css/images/COPVA.png",
+        "public/css/images/GRAD.png",
+        "public/css/images/IRS.png",
+        "public/css/images/NURSING.png",
+    ];
+?>
 @extends('layouts.master')
 
 @section('content')
@@ -7,36 +34,36 @@
             <h1> TALENT </h1>
         </div>
         <div class="row col-md-12">
-        <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
             <div class="tabs-vertical-env col-md-6">
                 <ul class="nav tabs-vertical">
-                    @for( $i=0 ; $i<=12 ; $i++)
+                    @for( $i=0 ; $i<=9 ; $i++)
                         @if($i==0)
                             <li class="active">
-                                <a href=#list-{{$i}} data-toggle="tab" aria-expanded="false">{{$i}}</a>
+                                <a href=#list-{{$i}} data-toggle="tab" aria-expanded="false">{{$collegeCode[$i]}}</a>
                             </li>
                         @else
                             <li class="">
-                                <a href=#list-{{$i}} data-toggle="tab" aria-expanded="false">{{$i}}</a>
+                                <a href=#list-{{$i}} data-toggle="tab" aria-expanded="false">{{$collegeCode[$i]}}</a>
                             </li>
                         @endif
                     @endfor
                 </ul>
                 <div class="tab-content" style="padding:100px">
-                    @for( $i=0 ; $i<=12 ; $i++)
+                    @for( $i=0 ; $i<=9 ; $i++)
                         @if($i==0)
                             <div class="tab-pane active width-auto" id=list-{{$i}}>
                                 <div class="card" style="padding : 20px">
                                     <div class="align-center">
                                         <img
                                             class="card-img-top"
-                                            src="public/css/images/testImg.jpg"
+                                            src={{$picSrc[$i]}}
                                             alt="Card image cap"
                                             style="height : 250px; width : 250px; border-radius : 50%">
                                     </div>
                                     <div class="card-body">
-                                        <h3 class="card-title">COLLEGE OF {{$i}}</h3>
-                                        <h5 class="card-title">NAME {{$i}}</h5>
+                                        <h3 class="card-title">{{$collegeCode[$i]}}</h3>
+                                        <h5 class="card-title">{{$name[$i]}}</h5>
                                         <div class="input-group input-group-lg">
                                         <div class="input-group-prepend">
                                             <span class="input-group-text" id="inputGroup-sizing-lg">SCORE</span>
@@ -52,13 +79,13 @@
                                     <div class="align-center">
                                         <img
                                             class="card-img-top"
-                                            src="public/css/images/testImg.jpg"
+                                            src={{$picSrc[$i]}}
                                             alt="Card image cap"
                                             style="height : 250px; width : 250px; border-radius : 50%">
                                     </div>
                                     <div class="card-body">
-                                        <h3 class="card-title">COLLEGE OF {{$i}}</h3>
-                                        <h5 class="card-title">NAME {{$i}}</h5>
+                                        <h3 class="card-title">{{$collegeCode[$i]}}</h3>
+                                        <h5 class="card-title">{{$name[$i]}}</h5>
                                         <div class="input-group input-group-lg">
                                         <div class="input-group-prepend">
                                             <span class="input-group-text" id="inputGroup-sizing-lg">SCORE</span>
@@ -73,8 +100,9 @@
                     <div class="col-md-2"></div>
                 </div>
             </div>
-        <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
         </div>
+        <button type="button" class="btn btn-link waves-effect">SAVE CHANGES</button>
     </div> <!-- container -->
 </section>
 
@@ -158,7 +186,3 @@
 </div>
 
 @endsection
-
-<script src="public/js/bootstrap.js"></script>
-<script>
-</script>

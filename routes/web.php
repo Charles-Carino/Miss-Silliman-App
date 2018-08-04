5253d8b267770987cb041f9d379a8162b79121ac
Dan <danjoaquinchin@gmail.com>
2018-08-04 19:30:45 +0800

    inclusion of 'home' page
    idk what to call it yet lmao

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('home');
+});
+
 Route::get('/tabulations_specialProjects', function () {
     return view('tabulations.tSP');
 });

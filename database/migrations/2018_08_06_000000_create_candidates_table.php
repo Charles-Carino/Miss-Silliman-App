c6cf688a10f4d1a3e901881281c948a9e2d6018e
Charles Cariño <charlescarino4@gmail.com>
2018-08-08 12:08:03 +0800

    Changes on the migrations

--- database/migrations/2018_08_06_000000_create_candidates_table.php
+++ database/migrations/2018_08_06_000000_create_candidates_table.php
@@ -17,14 +17,14 @@ class CreateCandidatesTable extends Migration
         Schema::create('candidates', function (Blueprint $table) {
             $table->increments('id');
             $table->string('image');
-            $table->string('C_FName'); //Candidate's First Name
-            $table->string('C_MName'); //Candidate's Middle Name
-            $table->string('C_LName'); //Candidate's Last Name
-            $table->integer('C_College')->unsigned(); //Candidate's College
-            $table->string('C_YearLevel'); //Candidate's Year Level
-            $table->string('C_SY'); //Candidate's SY attended
-            $table->string('C_isTop'); //Candidate's ranking
-            $table->string('C_Number'); //Candidate's number
+            $table->string('fName'); //Candidate's First Name
+            $table->string('mName'); //Candidate's Middle Name
+            $table->string('lName'); //Candidate's Last Name
+            $table->integer('college')->unsigned(); //Candidate's College
+            $table->string('yearLevel'); //Candidate's Year Level
+            $table->string('SY'); //Candidate's SY attended
+            $table->string('isTop'); //Candidate's ranking
+            $table->string('number'); //Candidate's number
             $table->timestamps();
 
 

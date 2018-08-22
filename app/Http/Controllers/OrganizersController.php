7af742fce71ac532617044f62e51f3350f3f6640
Charles Cariño <charlescarino4@gmail.com>
2018-08-22 09:46:07 +0800

    Final changes from the Prepageant.

--- app/Http/Controllers/OrganizersController.php
+++ app/Http/Controllers/OrganizersController.php
@@ -7,6 +7,7 @@ use App\Candidates;
 use App\Colleges;
 use App\Prepageants;
 use App\InitialScores;
+use App\PressLaunches;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\DB;
@@ -24,139 +25,144 @@ class OrganizersController extends Controller
         $initScores = InitialScores::join('candidates','initial_scores.candidate','=','candidates.id','left')
                                     ->join('colleges','candidates.college','=','colleges.id','left')
                                     ->get();
+        $pressLaunchScores = PressLaunches::join('users','users.id','=','press_launches.organizer','left')
+									->join('candidates','press_launches.candidate','=','candidates.id','left')
+									->join('colleges','candidates.college','=','colleges.id','left')
+									->orderby('PL_RS','desc')
+									->get();
 
+                  $reports = DB::select(DB::raw("select
+          			t0.id,
+          			t0.cCode,
+                      t0.candidates,
+                      t0.SP as 'SP',
+                  	t1.talent as 'judge1',
+                      t2.talent as 'judge2',
+                      t3.talent as 'judge3',
+                  	(t1.talent+t2.talent+t3.talent)/3 as AverageTalent,
+                  	t4.speech as 'judge4',
+                      t5.speech as 'judge5',
+                      t6.speech as 'judge6',
+                  	(t4.speech+t5.speech+t6.speech)/3 as AverageSpeech,
+                  	(t0.SP*.16) as 'SP_sixteen',
+                  	((t1.talent+t2.talent+t3.talent)/3)*.34 as 'Talent_34',
+                  	((t4.speech+t5.speech+t6.speech)/3)*.50 as 'Speech_50',
+                  	(t0.SP*.16)+((t1.talent+t2.talent+t3.talent)/3)*.34+((t4.speech+t5.speech+t6.speech)/3)*.50 as 'TotalPrepageant'
 
+                  from
+                  /*special project*/
+                  	(select can.id,concat(upper(can.lName),', ',can.fName) candidates
+                  		,pre.judge
+          				,sum(pre.SP_RS) SP
+          				,col.`collegeCode` cCode
+                  	from prepageants pre
+                  	left join candidates can on can.id=pre.candidate
+                  	left join colleges col on col.id=can.college
+                  	group by candidates
+                  	ORDER BY can.id) t0
+                  /*end*/
+                  	,
+                  /*talent judge1*/
+                  	(select can.id,concat(upper(can.lName),', ',can.fName) candidates
+                  		,pre.judge
+                  		,Talent_Confidence
+                  		,Talent_Mastery
+                  		,Talent_StagePresence
+                  		,Talent_OverallImpact
+                  		,(Talent_Confidence+Talent_Mastery+Talent_StagePresence+Talent_OverallImpact) talent
+                  	from prepageants pre
+                  	left join candidates can on can.id=pre.candidate
+                  	left join colleges col on col.id=can.college
+                  	WHERE pre.judge = 2
+                  	group by candidates
+                  	ORDER BY can.id) t1
+                  /*end*/
+                  	,
+                  /*talent judge2*/
+                  	(select can.id,concat(upper(can.lName),', ',can.fName) candidates
+                  		,pre.judge
+                  		,Talent_Confidence
+                  		,Talent_Mastery
+                  		,Talent_StagePresence
+                  		,Talent_OverallImpact
+                  		,(Talent_Confidence+Talent_Mastery+Talent_StagePresence+Talent_OverallImpact) talent
+                  	from prepageants pre
+                  	left join candidates can on can.id=pre.candidate
+                  	left join colleges col on col.id=can.college
+                  	WHERE pre.judge = 3
+                  	group by candidates
+                  	ORDER BY can.id) t2
+                  /*end*/
+                  	,
+                  /*talent judge3*/
+                  	(select can.id,concat(upper(can.lName),', ',can.fName) candidates
+                  		,pre.judge
+                  		,Talent_Confidence
+                  		,Talent_Mastery
+                  		,Talent_StagePresence
+                  		,Talent_OverallImpact
+                  		,(Talent_Confidence+Talent_Mastery+Talent_StagePresence+Talent_OverallImpact) talent
+                  	from prepageants pre
+                  	left join candidates can on can.id=pre.candidate
+                  	left join colleges col on col.id=can.college
+                  	WHERE pre.judge = 4
+                  	group by candidates
+                  	ORDER BY can.id) t3
+                  /*end*/
+                  	,
+                  /*speech judge4*/
+                  	(select can.id,concat(upper(can.lName),', ',can.fName) candidates
+                  		,pre.judge
+                  		,PSpch_Content
+                  		,PSpch_Delivery
+                  		,PSpch_Spontainety
+                  		,PSpch_Defense
+                  		,(PSpch_Content+PSpch_Delivery+PSpch_Spontainety+PSpch_Defense) speech
+                  	from prepageants pre
+                  	left join candidates can on can.id=pre.candidate
+                  	left join colleges col on col.id=can.college
+                  	WHERE pre.judge = 5 #replace with judge id ie: 4,5 or 6
+                  	group by candidates
+                  	ORDER BY can.id) t4
+                  /*end*/
+                  	,
+                  /*speech judge5*/
+                  	(select can.id,concat(upper(can.lName),', ',can.fName) candidates
+                  		,pre.judge
+                  		,PSpch_Content
+                  		,PSpch_Delivery
+                  		,PSpch_Spontainety
+                  		,PSpch_Defense
+                  		,(PSpch_Content+PSpch_Delivery+PSpch_Spontainety+PSpch_Defense) speech
+                  	from prepageants pre
+                  	left join candidates can on can.id=pre.candidate
+                  	left join colleges col on col.id=can.college
+                  	WHERE pre.judge = 6 #replace with judge id ie: 4,5 or 6
+                  	group by candidates
+                  	ORDER BY can.id) t5
+                  /*end*/
+                  	,
+                  /*speech judge6*/
+                  	(select can.id,concat(upper(can.lName),', ',can.fName) candidates
+                  		,pre.judge
+                  		,PSpch_Content
+                  		,PSpch_Delivery
+                  		,PSpch_Spontainety
+                  		,PSpch_Defense
+                  		,(PSpch_Content+PSpch_Delivery+PSpch_Spontainety+PSpch_Defense) speech
+                  	from prepageants pre
+                  	left join candidates can on can.id=pre.candidate
+                  	left join colleges col on col.id=can.college
+                  	WHERE pre.judge = 7 #replace with judge id ie: 4,5 or 6
+                  	group by candidates
+                  	ORDER BY can.id) t6
+                  /*end*/
 
-        $reports = DB::select(DB::raw("select
-        	t0.id,
-            t0.candidates,
-            t0.SP as 'SP',
-        	t1.talent as 'judge1',
-            t2.talent as 'judge2',
-            t3.talent as 'judge3',
-        	(t1.talent+t2.talent+t3.talent)/3 as AverageTalent,
-        	t4.speech as 'judge4',
-            t5.speech as 'judge5',
-            t6.speech as 'judge6',
-        	(t4.speech+t5.speech+t6.speech)/3 as AverageSpeech,
-        	(t0.SP*.16) as 'SP16%',
-        	((t1.talent+t2.talent+t3.talent)/3)*.34 as 'Talent34%',
-        	((t4.speech+t5.speech+t6.speech)/3)*.50 as 'Speech50%',
-        	(t0.SP*.16)+((t1.talent+t2.talent+t3.talent)/3)*.34+((t4.speech+t5.speech+t6.speech)/3)*.50 as 'TotalPrepageant'
+                  where
+                  	t0.candidates=t1.candidates and t1.candidates=t2.candidates and
+                  	t2.candidates=t3.candidates and t3.candidates=t4.candidates and
+                  	t4.candidates=t5.candidates and t5.candidates=t6.candidates"));
 
-        from
-        /*special project*/
-        	(select can.id,concat(upper(can.lName),', ',can.fName,' - ',col.`collegeCode`) candidates
-        		,pre.judge
-        		,sum(pre.SP_RS) SP
-        	from prepageants pre
-        	left join candidates can on can.id=pre.candidate
-        	left join colleges col on col.id=can.college
-        	group by candidates
-        	ORDER BY can.id) t0
-        /*end*/
-        	,
-        /*talent judge1*/
-        	(select can.id,concat(upper(can.lName),', ',can.fName,' - ',col.`collegeCode`) candidates
-        		,pre.judge
-        		,Talent_Confidence
-        		,Talent_Mastery
-        		,Talent_StagePresence
-        		,Talent_OverallImpact
-        		,(Talent_Confidence+Talent_Mastery+Talent_StagePresence+Talent_OverallImpact) talent
-        	from prepageants pre
-        	left join candidates can on can.id=pre.candidate
-        	left join colleges col on col.id=can.college
-        	WHERE pre.judge = 2
-        	group by candidates
-        	ORDER BY can.id) t1
-        /*end*/
-        	,
-        /*talent judge2*/
-        	(select can.id,concat(upper(can.lName),', ',can.fName,' - ',col.`collegeCode`) candidates
-        		,pre.judge
-        		,Talent_Confidence
-        		,Talent_Mastery
-        		,Talent_StagePresence
-        		,Talent_OverallImpact
-        		,(Talent_Confidence+Talent_Mastery+Talent_StagePresence+Talent_OverallImpact) talent
-        	from prepageants pre
-        	left join candidates can on can.id=pre.candidate
-        	left join colleges col on col.id=can.college
-        	WHERE pre.judge = 3
-        	group by candidates
-        	ORDER BY can.id) t2
-        /*end*/
-        	,
-        /*talent judge3*/
-        	(select can.id,concat(upper(can.lName),', ',can.fName,' - ',col.`collegeCode`) candidates
-        		,pre.judge
-        		,Talent_Confidence
-        		,Talent_Mastery
-        		,Talent_StagePresence
-        		,Talent_OverallImpact
-        		,(Talent_Confidence+Talent_Mastery+Talent_StagePresence+Talent_OverallImpact) talent
-        	from prepageants pre
-        	left join candidates can on can.id=pre.candidate
-        	left join colleges col on col.id=can.college
-        	WHERE pre.judge = 4
-        	group by candidates
-        	ORDER BY can.id) t3
-        /*end*/
-        	,
-        /*speech judge4*/
-        	(select can.id,concat(upper(can.lName),', ',can.fName,' - ',col.`collegeCode`) candidates
-        		,pre.judge
-        		,PSpch_Content
-        		,PSpch_Delivery
-        		,PSpch_Spontainety
-        		,PSpch_Defense
-        		,(PSpch_Content+PSpch_Delivery+PSpch_Spontainety+PSpch_Defense) speech
-        	from prepageants pre
-        	left join candidates can on can.id=pre.candidate
-        	left join colleges col on col.id=can.college
-        	WHERE pre.judge = 5 #replace with judge id ie: 4,5 or 6
-        	group by candidates
-        	ORDER BY can.id) t4
-        /*end*/
-        	,
-        /*speech judge5*/
-        	(select can.id,concat(upper(can.lName),', ',can.fName,' - ',col.`collegeCode`) candidates
-        		,pre.judge
-        		,PSpch_Content
-        		,PSpch_Delivery
-        		,PSpch_Spontainety
-        		,PSpch_Defense
-        		,(PSpch_Content+PSpch_Delivery+PSpch_Spontainety+PSpch_Defense) speech
-        	from prepageants pre
-        	left join candidates can on can.id=pre.candidate
-        	left join colleges col on col.id=can.college
-        	WHERE pre.judge = 6 #replace with judge id ie: 4,5 or 6
-        	group by candidates
-        	ORDER BY can.id) t5
-        /*end*/
-        	,
-        /*speech judge6*/
-        	(select can.id,concat(upper(can.lName),', ',can.fName,' - ',col.`collegeCode`) candidates
-        		,pre.judge
-        		,PSpch_Content
-        		,PSpch_Delivery
-        		,PSpch_Spontainety
-        		,PSpch_Defense
-        		,(PSpch_Content+PSpch_Delivery+PSpch_Spontainety+PSpch_Defense) speech
-        	from prepageants pre
-        	left join candidates can on can.id=pre.candidate
-        	left join colleges col on col.id=can.college
-        	WHERE pre.judge = 7 #replace with judge id ie: 4,5 or 6
-        	group by candidates
-        	ORDER BY can.id) t6
-        /*end*/
-
-        where
-        	t0.candidates=t1.candidates and t1.candidates=t2.candidates and
-        	t2.candidates=t3.candidates and t3.candidates=t4.candidates and
-        	t4.candidates=t5.candidates and t5.candidates=t6.candidates"));
-          
-        return view('maintenance.maintenance',compact('judges','organizers','candidates', 'colleges','prepageants','initScores','prePajFinal', 'reports'));
+        return view('maintenance.maintenance',compact('judges','organizers','candidates', 'colleges','prepageants','initScores','prePajFinal', 'reports','pressLaunchScores'));
       }
 }
